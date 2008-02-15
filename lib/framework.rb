2ad75e0d830bd62c1310d150590c37c42e5c51f2
Ken Pratt <ken@kenpratt.net>
2008-02-15 13:10:39 -0800

    Revamped routing and controllers. - Added routing mechanism to request handler (instead of registering each controller with mongrel) - Controllers are now defined using a method which takes in routes and injects them into a Controller class - Reorganized code into modules

--- lib/framework.rb
+++ lib/framework.rb
@@ -2,97 +2,19 @@
 
 %w{ config rubygems fileutils mongrel }.each {|l| require l }
 
-class RequestHandler < Mongrel::HttpHandler
-  def initialize(*uri_formats)
-    @uri_formats = uri_formats
-  end
+def escape(s); Mongrel::HttpRequest.escape(s); end
+def unescape(s); Mongrel::HttpRequest.unescape(s); end
 
-  def process(request, response)
-    begin
-      @request, @response = request, response
-      args = extract_args(@request.params['REQUEST_URI'])
-      http_method = @request.params['REQUEST_METHOD'] || ''
-      log "#{http_method} #{@request.params['REQUEST_URI']}"
-      
-      case http_method.upcase
-      when 'GET'
-        get(*args)
-      when 'POST'
-        @input = hashify(io_to_string(@request.body))
-        post(*args)
-      else
-        raise ArgumentError.new("Only GET and POST are supported, not '#{http_method}'")
-      end
-    rescue Exception => e
-      log "Error occured:"
-      log "#{e.class}: #{e.message}"
-      log e.backtrace.collect {|s| "        #{s}\n" }.join
-      @response.start(500) do |head, out|
-        head['Content-Type'] = 'text/html'
-        out.write '<pre>'
-        out.write "#{e.class}: #{e.message}\n"
-        out.write e.backtrace.collect {|s| "        #{s}\n" }.join
-        out.write '</pre>'
-      end
-    end
-  end
-  
-  def extract_args(request_uri)
-    log "Extracting arguments from '#{request_uri}'"
-    @uri_formats.each do |fmt|
-      log "  Attempting to match /^#{fmt}\\/?$/"
-      if m = /^#{fmt}\/?$/.match(request_uri)
-        log "    Found #{m.size - 1} arguments"
-        return m.to_a[1..-1].collect {|a| RequestHandler.unescape(a) }
-      end
-    end
-    []
-  end
-  
-  def hashify(str, hash={})
-    (str || '').split(/[&;] */n).each { |f| hash.store(*RequestHandler.unescape(f).split('=', 2)) }
-    hash
-  end
-  
-  def io_to_string(input)
-    if input.is_a?(Tempfile)
-      open(input.path).read
-    elsif input.is_a?(StringIO)
-      input.read
-    elsif input.is_a?(String)
-      input
-    else
-      raise ArgumentError.new("don't know how to read a #{input.class}")
-    end
-  end
-  
-  def self.escape(s); Mongrel::HttpRequest.escape(s); end
-  def self.unescape(s); Mongrel::HttpRequest.unescape(s); end
-  
-  def redirect(uri)
-    log "Redirecting to #{uri}"
-    @response.start(303) do |head, out|
-      head['Location'] = uri
-    end
-  end
-  
-  def render(template, context={})
-    log "Rendering #{template}"
-    @response.start(200) do |head, out|
-      head['Content-Type'] = 'text/html'
-      inner = process_template(template, context)
-      context.store(:inner, inner)
-      out.write process_template('structure', context)
-    end
-  end
-  
-  def process_template(template, context)
-    Erubis::Eruby.new(open(template_path(template)).read).evaluate(context)
-  end
-  
-  def template_path(template)
-    File.join(Conf::TEMPLATE_DIR, "#{template}.rhtml")
-  end
+# never subclass Controller directly! instead, use this method which creates a 
+# subclass with embedded route information
+def controller(*routes)
+  c = Class.new(Framework::Controller)
+  c.class_eval { define_method(:routes) { routes } }
+  c
+end
+
+def log(s)
+  puts s
 end
 
 def save_file(input, destination)
@@ -112,28 +34,191 @@ def save_file(input, destination)
   end
 end
 
-def log(s)
-  puts s
-end
+# Framework classes
+module Framework
 
-class Server
-  def initialize(addr, port, routes)
-    @addr, @port, @routes = addr, port, routes
-  end
-  
-  def start
-    h = Mongrel::HttpServer.new(@addr, @port)
-    @routes.each do |path, action|
-      case action
-      when String
-        h.register(path, Mongrel::RedirectHandler.new(action))
-      else
-        h.register(path, action)
+  # base controller class
+  # - methods extending this should implement get and/or post methods
+  # - this class should never be subclassed directly! instead, use the
+  #   controller(*routes) method
+  class Controller
+    def redirect(uri)
+      log "Redirecting to #{uri}"
+      @response.start(303) do |head, out|
+        head['Location'] = uri
       end
     end
-    h.register('/static', Mongrel::DirHandler.new('static/'))
-    h.register('/favicon.ico', Mongrel::Error404Handler.new(''))
-    puts "** #{Conf::APP_NAME} is now running at http://#{@addr}:#{@port}/"
-    h.run.join
+  
+    def render(template, context={})
+      log "Rendering #{template}"
+      @response.start(200) do |head, out|
+        head['Content-Type'] = 'text/html'
+        inner = process_template(template, context)
+        context.store(:inner, inner)
+        out.write process_template('structure', context)
+      end
+    end
+  
+    def process_template(template, context)
+      Erubis::Eruby.new(open(template_path(template)).read).evaluate(context)
+    end
+  
+    def template_path(template)
+      File.join(Conf::TEMPLATE_DIR, "#{template}.rhtml")
+    end
+  end
+
+  # router class 
+  # - handlers all the request routing and argument parsing
+  class Router
+    class NoPathFound < RuntimeError; end
+  
+    def initialize(controller_module)
+      add_controllers(Router.find_controllers(controller_module))
+    end
+
+    class << self
+      # given a module containing controllers, inspect the constants and return controller instances
+      def find_controllers(controller_module)
+        controller_module.constants.map do |c|
+          eval("#{controller_module}::#{c}")
+        end.select do |c|
+          c.kind_of? Class
+        end.map do |c|
+          c.new
+        end.select do |c|
+          c.kind_of? Controller
+        end
+      end
+    
+      def regex(route)
+        /^#{route}\/?$/
+      end
+    
+      def extract_arguments(uri, regex)
+        log "Extracting arguments from '#{uri}'"
+        log "  Attempting to match #{regex}"
+        if m = regex.match(uri)
+          log "    Found #{m.size - 1} arguments"
+          return m.to_a[1..-1].collect {|a| unescape(a) }
+        end
+        []
+      end
+    end
+
+    # add the given controller instances to the routing table
+    def add_controllers(controllers)
+      @route_map ||= {}
+      log "Router: adding controllers to route map: #{controllers.join(',')}"
+      controllers.each { |c| c.routes.each {|r| @route_map[r] = c } }
+      build_index
+    end
+
+    # priotize the routes in the routing table (keep an array of sorted keys to the route_map hash table)
+    def build_index
+      @routes = @route_map.keys.sort
+    end
+  
+    # process a given uri, returning the controller instance and extracted uri arguments
+    def process(uri)
+      log "Router: attempting to match #{uri}"
+      @routes.each do |r|
+        regex = Router.regex(r)
+        log "  trying #{regex}"
+        if regex.match(uri)
+          # route r is correct
+          controller = @route_map[r]
+          args = Router.extract_arguments(uri, regex)
+          log "    success! controller is #{controller}, args are #{args.join(', ')}"
+          return controller, args
+        end
+      end
+      raise NoPathFound
+    end
+  
+  end
+
+  # request handler
+  # - wraps mongrel HttpHandler and interacts with Router and Controllers
+  class RequestHandler < Mongrel::HttpHandler
+    def initialize(controller_module)
+      @router = Router.new(controller_module)
+    end
+
+    def process(request, response)
+      begin
+        http_method, request_uri = request.params['REQUEST_METHOD'], request.params['REQUEST_URI']
+        log "#{http_method} #{request_uri}"
+        controller, args = @router.process(request_uri)
+        
+        # TODO instead of injecting instance variables, can we use metaprogramming 
+        # to define get/post methods that have response and input as args?
+        
+        # inject the response object
+        controller.instance_variable_set("@response", response)
+        
+        case http_method.upcase
+        when 'GET'
+          # call controller get method
+          controller.get(*args)
+        when 'POST'
+          # inject input object
+          controller.instance_variable_set("@input", hashify(io_to_string(request.body)))
+        
+          # call controller post method
+          controller.post(*args)
+        else
+          raise ArgumentError.new("Only GET and POST are supported, not '#{http_method}'")
+        end
+      
+      rescue Exception => e
+        log "Error occured:"
+        log "#{e.class}: #{e.message}"
+        log e.backtrace.collect {|s| "        #{s}\n" }.join
+        @response.start(500) do |head, out|
+          head['Content-Type'] = 'text/html'
+          out.write '<pre>'
+          out.write "#{e.class}: #{e.message}\n"
+          out.write e.backtrace.collect {|s| "        #{s}\n" }.join
+          out.write '</pre>'
+        end
+      end
+    end
+  
+    def hashify(str, hash={})
+      (str || '').split(/[&;] */n).each { |f| hash.store(*unescape(f).split('=', 2)) }
+      hash
+    end
+
+    def io_to_string(input)
+      if input.is_a?(Tempfile)
+        open(input.path).read
+      elsif input.is_a?(StringIO)
+        input.read
+      elsif input.is_a?(String)
+        input
+      else
+        raise ArgumentError.new("don't know how to read a #{input.class}")
+      end
+    end
+  end
+  
+  # server
+  # - defines a mongrel http server for the app
+  # - static requests are handled by mongrel
+  # - other requests are handled by RequestHandler
+  class Server
+    def initialize(addr, port, controller_module)
+      @addr, @port, @controller_module = addr, port, controller_module
+    end
+  
+    def start
+      h = Mongrel::HttpServer.new(@addr, @port)
+      h.register('/', RequestHandler.new(@controller_module))
+      h.register('/static', Mongrel::DirHandler.new('static/'))
+      h.register('/favicon.ico', Mongrel::Error404Handler.new(''))
+      puts "** #{Conf::APP_NAME} is now running at http://#{@addr}:#{@port}/"
+      h.run.join
+    end
   end
 end
--- lib/satellite.rb
+++ lib/satellite.rb
@@ -9,215 +9,197 @@
 
 module Satellite
 
-  # "Page" is a model representing a wiki page
-  # Pages are saved locally in the filesystem, changes are committed to a local
-  # Git repository which is mirrored to a master repository
-  class Page
-    include Comparable
+  # model definitions go here
+  module Models
+
+    # "Page" is a model representing a wiki page
+    # Pages are saved locally in the filesystem, changes are committed to a local
+    # Git repository which is mirrored to a master repository
+    class Page
+      include Comparable
     
-    VALID_NAME_CHARS = '\w \!\@\#\$\%\^\&\(\)\-\_\+\=\[\]\{\}\,\.'
-    WIKI_LINK_FMT = /\{\{([#{VALID_NAME_CHARS}]+)\}\}/
+      VALID_NAME_CHARS = '\w \!\@\#\$\%\^\&\(\)\-\_\+\=\[\]\{\}\,\.'
+      WIKI_LINK_FMT = /\{\{([#{VALID_NAME_CHARS}]+)\}\}/
   
-    PAGE_DIR = 'pages'
-    PAGE_PATH = File.join(Conf::DATA_DIR, PAGE_DIR)
+      PAGE_DIR = 'pages'
+      PAGE_PATH = File.join(Conf::DATA_DIR, PAGE_DIR)
     
-    # static methods
-    class << self
-      def list
-        Dir[filepath('*')].collect {|s| s.sub(/^#{PAGE_PATH}\/(.+)\.textile$/, '\1') }.collect {|s| Page.new(s) }.sort
-      end
-
-      def load(name)
-        if exists?(name)
-          Page.new(name, open(filepath(name)).read)
-        else
-          nil
-        end
-      end
-
-      def exists?(name)
-        File.exists?(filepath(name))
-      end
-
-      # "foo.textile"
-      def filename(name); "#{name}.textile"; end
-
-      # "path/to/foo.textile"
-      def filepath(name); File.join(PAGE_PATH, filename(name)); end
-    end
-  
-    # instance methods
-    attr_reader :name
-    attr_writer :body
-
-    def initialize(name='', body='')
-      @name = name
-      @body = body
-      raise ArgumentError.new("Name is invalid: #{name}") if name.any? && !valid_name?
-    end
-
-    def body(format=nil)
-      case format
-      when :html
-        to_html
-      else
-        @body
-      end
-    end
-  
-    def save
-      begin
-        save_file(@body, filepath)
-        relative_path = File.join(PAGE_DIR, filename)
-        Db.save(relative_path, "Satellite: saving #{name}")
-      rescue Db::ContentNotModified
-        log "Didn't need to save #{name}"
-      end
-    end
-  
-    def valid_name?
-      name =~ /^[#{VALID_NAME_CHARS}]+$/
-    end
-    
-    # sort home above other pages, otherwise alphabetical order
-    def <=>(other)
-      if name == 'Home'
-        -1
-      elsif other.name == 'Home'
-        1
-      else
-        name <=> other.name
-      end
-    end
-    
-    def to_html
-      str = @body
-      
-      # wiki linking
-      str = str.gsub(WIKI_LINK_FMT) do |s|
-        name, uri = $1, WikiController::Uri.page($1)
-        notextile do
-          if Page.exists?(name)
-            "<a href=\"#{uri}\">#{name}</a>"
-          else
-            "<span class=\"nonexistant\">#{name}<a href=\"#{uri}\">?</a></span>"
-          end 
-        end
-      end
-      
-      # textile -> html filtering
-      RedCloth.new(str).to_html
-    end
-  
-    def filename; Page.filename(name); end
-    def filepath; Page.filepath(name); end
-    
-    # helper to wrap wrap block in notextile tags (block should return html string)
-    def notextile
-      str = yield
-      "<notextile>#{str.to_s}</notextile>" if str && str.any?
-    end
-  end
-
-  # The wiki controller just wraps the base framework mongrel request handler
-  # to provide some
-  class WikiController < RequestHandler
-    VALID_CHARS = '\w \+\%\-\.'
-    NAME = "([#{VALID_CHARS}]+)"
-  
-    alias :original_render :render
-    def render(template, title, params={})
-      original_render(template, params.merge!({ :title => title, :uri => Uri }))
-    end
-  
-    class Uri
+      # static methods
       class << self
-        def page(name) "/page/#{RequestHandler.escape(name)}" end
-        def edit_page(name) "/page/#{RequestHandler.escape(name)}/edit" end
-        def new_page() '/new' end
-        def list() '/list' end
-        def home() '/page/Home' end
-      end
-    end
-  end
-
-  # TODO it would be nice to have this be like camping, with something like < R "/page/(.+)"
-  # instead of the inherit block. the necessary method might look like:
-  #
-  # see: http://whytheluckystiff.net/articles/seeingMetaclassesClearly.html
-  # def R(uri_format)
-  #   Class.new(RequestHandler) do
-  #     meta_def(:uri_format) { uri_format }
-  #   end
-  # end
-  #
-  class PageController < WikiController
-    def initialize
-      super "/page/#{NAME}", "/page/#{NAME}/(edit)"
-    end
-  
-    def get(name, action='view')
-      page = Page.load(name)
-      case action
-      when 'view'
-        if page
-          render 'show_page', page.name, :page => page
-        else
-          redirect Uri.edit_page(name)
+        def list
+          Dir[filepath('*')].collect {|s| s.sub(/^#{PAGE_PATH}\/(.+)\.textile$/, '\1') }.collect {|s| Page.new(s) }.sort
         end
-      when 'edit'
-        page ||= Page.new(name)
-        render 'edit_page', "Editing #{page.name}", :page => page
+
+        def load(name)
+          if exists?(name)
+            Page.new(name, open(filepath(name)).read)
+          else
+            nil
+          end
+        end
+
+        def exists?(name)
+          File.exists?(filepath(name))
+        end
+
+        # "foo.textile"
+        def filename(name); "#{name}.textile"; end
+
+        # "path/to/foo.textile"
+        def filepath(name); File.join(PAGE_PATH, filename(name)); end
       end
-    end
   
-    def post(name, action=nil)
-      page = Page.new(name, @input['content'])
-      page.save
-      redirect Uri.page(page.name)
+      # instance methods
+      attr_reader :name
+      attr_writer :body
+
+      def initialize(name='', body='')
+        @name = name
+        @body = body
+        raise ArgumentError.new("Name is invalid: #{name}") if name.any? && !valid_name?
+      end
+
+      def body(format=nil)
+        case format
+        when :html
+          to_html
+        else
+          @body
+        end
+      end
+  
+      def save
+        begin
+          save_file(@body, filepath)
+          relative_path = File.join(PAGE_DIR, filename)
+          Db.save(relative_path, "Satellite: saving #{name}")
+        rescue Db::ContentNotModified
+          log "Didn't need to save #{name}"
+        end
+      end
+  
+      def valid_name?
+        name =~ /^[#{VALID_NAME_CHARS}]+$/
+      end
+    
+      # sort home above other pages, otherwise alphabetical order
+      def <=>(other)
+        if name == 'Home'
+          -1
+        elsif other.name == 'Home'
+          1
+        else
+          name <=> other.name
+        end
+      end
+    
+      def to_html
+        str = @body
+      
+        # wiki linking
+        str = str.gsub(WIKI_LINK_FMT) do |s|
+          name, uri = $1, Framework::Controller::Uri.page($1)
+          notextile do
+            if Page.exists?(name)
+              "<a href=\"#{uri}\">#{name}</a>"
+            else
+              "<span class=\"nonexistant\">#{name}<a href=\"#{uri}\">?</a></span>"
+            end 
+          end
+        end
+      
+        # textile -> html filtering
+        RedCloth.new(str).to_html
+      end
+  
+      def filename; Page.filename(name); end
+      def filepath; Page.filepath(name); end
+    
+      # helper to wrap wrap block in notextile tags (block should return html string)
+      def notextile
+        str = yield
+        "<notextile>#{str.to_s}</notextile>" if str && str.any?
+      end
     end
   end
 
-  class NewPageController < WikiController
-    def initialize
-      super '/new'
-    end
+  # controllers definitions go here
+  module Controllers
+    
+    VALID_PAGE_NAME_CHARS = '\w \+\%\-\.'
+    PAGE_NAME = "([#{VALID_PAGE_NAME_CHARS}]+)"
+
+    # reopen controller class to provide some app-specific logic
+    class Framework::Controller
+      
+      # pass title and uri mappings into templates too
+      alias :original_render :render
+      def render(template, title, params={})
+        original_render(template, params.merge!({ :title => title, :uri => Uri }))
+      end
   
-    def get
-      render 'new_page', 'Add page', :page => Page.new
+      # uri mappings
+      # TODO somehow generate these from routing table?
+      class Uri
+        class << self
+          def page(name) "/page/#{escape(name)}" end
+          def edit_page(name) "/page/#{escape(name)}/edit" end
+          def new_page() '/new' end
+          def list() '/list' end
+          def home() '/page/Home' end
+        end
+      end
     end
+
+    class PageController < controller "/page/#{PAGE_NAME}", "/page/#{PAGE_NAME}/(edit)"
+      def get(name, action='view')
+        page = Models::Page.load(name)
+        case action
+        when 'view'
+          if page
+            render 'show_page', page.name, :page => page
+          else
+            redirect Uri.edit_page(name)
+          end
+        when 'edit'
+          page ||= Models::Page.new(name)
+          render 'edit_page', "Editing #{page.name}", :page => page
+        end
+      end
   
-    def post
-      page = Page.new(@input['name'].strip, @input['content'])
-      unless Page.exists?(page.name)
+      def post(name, action=nil)
+        page = Models::Page.new(name, @input['content'])
         page.save
         redirect Uri.page(page.name)
-      else
-        render 'new_page', 'Add page', :page => page, :error => "A page named #{page.name} already exists"
+      end
+    end
+
+    class NewPageController < controller '/new'
+      def get
+        render 'new_page', 'Add page', :page => Models::Page.new
+      end
+  
+      def post
+        page = Models::Page.new(@input['name'].strip, @input['content'])
+        unless Models::Page.exists?(page.name)
+          page.save
+          redirect Uri.page(page.name)
+        else
+          render 'new_page', 'Add page', :page => page, :error => "A page named #{page.name} already exists"
+        end
+      end
+    end
+
+    class ListController < controller '/list'
+      def get
+        render 'list_pages', 'All pages', :pages => Models::Page.list
       end
     end
   end
-
-  class ListController < WikiController
-    def initialize
-      super '/list'
-    end
-  
-    def get
-      render 'list_pages', 'All pages', :pages => Page.list
-    end
-  end
-
-  ROUTES = [ 
-    [ '/', '/page/Home' ],
-    [ '/page', PageController.new ],
-    [ '/new', NewPageController.new ],
-    [ '/list', ListController.new ]
-  ]
 
   class << self
     def start
-      Server.new(Conf::SERVER_IP, Conf::SERVER_PORT, ROUTES).start
+      Framework::Server.new(Conf::SERVER_IP, Conf::SERVER_PORT, Controllers).start
     end
   end
 end
