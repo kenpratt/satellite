65fed9e2266454c74d301ed7da3d4ecd6384381f
Ken Pratt <ken@kenpratt.net>
2008-03-22 14:28:55 -0700

    Added hotkeys script, with e for edit on wiki pages.

---
+++ static/hotkeys.js
@@ -0,0 +1,78 @@
+var HotKeys = {
+	'actions': {},
+
+	'add': function(key, func) {
+		this.actions[key] = func;
+	},
+
+	'listener': function(e) {
+		e = e || window.event;
+
+		// no hotkeys in text fields
+		var element = getElement(e);
+		if (element) {
+			var tag = element.tagName.toLowerCase();
+			if (tag == 'input' || tag == 'textarea') {
+				return;
+			}
+		}	
+
+		// get key code
+		var code;
+		if (e.keyCode) {
+			code = e.keyCode;
+		} else if (e.which) {
+			code = e.which;
+		}
+
+		// perform action for hotkey
+		if (code) {
+			var key = String.fromCharCode(code).toLowerCase();
+			var func = HotKeys.actions[key];
+			if (func) {
+				func();
+			}
+		}
+	},
+	
+	'attach': function() {
+	  attachListener(document, 'keydown', HotKeys.listener);
+	}
+};
+
+function getElement(e) {
+	e = e || window.event;
+
+	var element;
+	if (e.target) {
+		element = e.target;
+	} else if (e.srcElement) {
+		element = e.srcElement;
+	}
+
+	if (element.nodeType == 3) {
+		element = element.parentNode;
+	}
+	
+	return element;
+}
+
+function attachListener(target, eventType, func) {
+	// target can be an elem or an id
+	if (typeof target == 'string') {
+		target = document.getElementById(target);
+	}
+
+	// add listener
+	if (target.addEventListener) {
+		target.addEventListener(eventType, func, false);
+	} else if (target.attachEvent) {
+		target.attachEvent('on'+eventType, func);
+	} else {
+		target['on'+eventType] = func;
+	}
+}
+
+function redirect(uri) {
+  return function() { window.location = uri };
+}
