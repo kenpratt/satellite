7ab5e274e65ef0007f0e8c3e64c4d8e2d6b04a65
Ken Pratt <ken@kenpratt.net>
2008-08-03 04:04:56 -0700

    Added profiler.

---
+++ conf/profiling.rb
@@ -0,0 +1,14 @@
+# production configuration
+
+# URI of master git repository for wiki content
+config.master_repository_uri = 'git+ssh://kenpratt@mueller.kenpratt.net/var/git/wiki.kenpratt.net'
+
+# user information to use for git content submissions
+config.user_name = 'Ken Pratt'
+config.user_email = 'ken@kenpratt.net'
+
+# logging level (:error, :warn, :info, :debug)
+config.log_level = :error
+
+# automatically reload app when app files change? (for development)
+config.auto_reload = false
